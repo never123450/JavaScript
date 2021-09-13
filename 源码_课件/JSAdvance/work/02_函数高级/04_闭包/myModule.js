061f32b00be0a17eb9934daff304f5d44b1f039a
845619585@qq.com <845619585@qq.com>
2021-09-13 15:48:46 +0800

    格式化

--- 源码_课件/JSAdvance/work/02_函数高级/04_闭包/myModule.js
+++ 源码_课件/JSAdvance/work/02_函数高级/04_闭包/myModule.js
@@ -3,10 +3,10 @@ function myModule() {
   var msg = 'My atguigu'
   //操作数据的函数
   function doSomething() {
-    console.log('doSomething() '+msg.toUpperCase())
+    console.log('doSomething() ' + msg.toUpperCase())
   }
-  function doOtherthing () {
-    console.log('doOtherthing() '+msg.toLowerCase())
+  function doOtherthing() {
+    console.log('doOtherthing() ' + msg.toLowerCase())
   }
 
   //向外暴露对象(给外部使用的方法)
